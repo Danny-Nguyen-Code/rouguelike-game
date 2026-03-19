d953083d31ad9dccfd75dc02a48939b3beaa10e4
Danny Nguyen <204911897+Danny-Nguyen-Code@users.noreply.github.com>
2026-03-19 10:36:34 -0700

    Fix bug in tile_types.py
    In line 28, change "transparent, into" -> "transparent: into"

--- tile_types.py
+++ tile_types.py
@@ -6,7 +6,7 @@ import numpy as np
 # dtype creates data type that numpy can use, behaves similarly to struct in C
 graphic_dt = np.dtype(
     [
-        ("ch", np,int32),   # Character, translated from int to Unicode
+        ("ch", np.int32),   # Character, translated from int to Unicode
         ("fg", "3B"),   # Foreground color, 3 unsigned bytes, for RGB colors
         ("bg", "3B"),   # Background color, similar to fg
     ]
@@ -25,15 +25,17 @@ tile_dt = np.dtype(
 def new_tile(
     *,  # Enforce use of keywords, so parameter order doesn't matter
     walkable: int,
-    transparent, int,
+    transparent: int,
     dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:
-    """Returns numpy array of one tile_dt element and returns it"""
+    """Helper function for defining individual tile types.
+    Returns numpy array of one tile_dt element and returns it"""
     return np.array((walkable, transparent, dark), dtype=tile_dt)
 
 floor = new_tile(
     walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
 )
+
 wall = new_tile(
     walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
 )
