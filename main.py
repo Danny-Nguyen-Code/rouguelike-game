292a9bd03a56c6deabd0b677305d492bf99f9bc9
Danny Nguyen <204911897+Danny-Nguyen-Code@users.noreply.github.com>
2026-03-18 20:10:36 -0700

    Add entity.py and add to main.py

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ import tcod
 
 from actions import EscapeAction, MovementAction
 from input_handlers import EventHandler
+from entity import Entity
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
-
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
     #receive events and process them
     event_handler = EventHandler()
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    entities = {npc, player}
 
     with tcod.context.new_terminal(
         screen_width,
@@ -26,7 +28,7 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=player_x, y=player_y, string="@")
+            root_console.print(x=player.x, y=player.y, string=player.char, fg=player.color)
 
             context.present(root_console)
             root_console.clear() #clear leftover @
@@ -39,8 +41,7 @@ def main() -> None:
                     continue
                 #if action is instance of MovementAction, move @ symbol
                 if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
+                    player.move(dx=action.dx, dy=action.dy)
                 elif isistance(action, EscapeAction):
                     raise SystemExit()
 
