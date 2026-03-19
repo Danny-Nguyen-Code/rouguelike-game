9e973565d5a651300f70fdb3e43d768c4b38a79e
Danny Nguyen <204911897+Danny-Nguyen-Code@users.noreply.github.com>
2026-03-19 10:44:26 -0700

    Fix bug in input_handlers.py
    In line 27, change "tcode.event.K_ESCAPE" -> "tcod.event.K_ESCAPE"

--- input_handlers.py
+++ input_handlers.py
@@ -24,7 +24,7 @@ class EventHandler(tcod.event.EventDispatch[Action]):
             action = MovementAction(dx=-1, dy=0)
         elif key == tcod.event.K_RIGHT:
             action = MovementAction(dx=1, dy=0)
-        elif key == tcode.event.K_ESCAPE:
+        elif key == tcod.event.K_ESCAPE:
             action = EscapeAction()
 
         return action
